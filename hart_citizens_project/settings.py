8c066ce0fcc32cbcc5e0c51ab6dc42abb5710bde
gazinur <lama2x2@yandex.ru>
2025-10-05 18:32:24 +0300

    fix deployment 0.8

--- hart_citizens_project/settings.py
+++ hart_citizens_project/settings.py
@@ -178,7 +178,7 @@ STATIC_URL = "static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.UUIDField"
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Custom User Model
 AUTH_USER_MODEL = "users.User"
